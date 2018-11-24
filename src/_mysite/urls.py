8a1488bcbc1d926b6c24f41b3e270bb9cafb3089
G <ghanemite@users.noreply.github.com>
2018-11-24 14:07:56 +0200

    item_undone in urls

--- src/_mysite/urls.py
+++ src/_mysite/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^list/create/$', CreateTodoListView.as_view(), name='create_todo_list'),
     url(r'^list/(?P<slug>[\w\-]+)/$', list_details, name='list_details'),
     url(r'^item/(?P<pk>\d+)/done/$', item_done, name='item_done'),
-    url(r'^item/(?P<pk>\d+)/undone/$', item_redone, name='item_undone'),
+    url(r'^item/(?P<pk>\d+)/undone/$', item_undone, name='item_undone'),
 
     # url(r'^item/(?P<pk>\d+)/undone/$', , name='item_done'),
     url(r'^list/(?P<slug>[\w\-]+)/add-item/$', AddItemViewView.as_view(), name='add_item'),
